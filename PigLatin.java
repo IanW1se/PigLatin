91819d3b860bd9db1eaf9ad5dc96312df86120a3
Collin Holmquist <collinholmquist@gmail.com>
2020-11-11 10:51:14 -0600

    Create PigLatin.java

---
+++ PigLatin.java
@@ -0,0 +1,18 @@
+public class PigLatin {
+
+  public static void main(String [] args){
+  
+  
+  }
+  
+  public static int findFirstVowel(String word) {
+  
+    return -1; 
+  }
+  
+  public static String pigLatin(String word) {
+  
+    return "";
+  }
+
+}
